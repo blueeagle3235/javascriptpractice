5247ca7508c3de387a40430007d35b56eaa17be2
blueeagle3235 <aaronlegomaster374@gmail.com>
2020-08-23 20:36:25 -0700

    minor sorting, tried to do much more, failed ;(

---
+++ public/phaser/spacesim/js/game.js
@@ -0,0 +1,44 @@
+// var config = {
+//     //type: Phaser.AUTO,
+//     type: Phaser.AUTO,
+//     scale: {
+//         mode: Phaser.Scale.FIT,
+//         parent: 'phaser-example',
+//         autoCenter: Phaser.Scale.CENTER_BOTH,
+//         width: 800,
+//         height: 600
+//     },
+//     //width: 800,
+//     //height: 600,
+//     //parent: 'phaser-example',
+//     /*scale: {
+//         parent: 'phaser-example',
+//         width: 800,
+//         height: 600
+//     },*/
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             debug: true
+//         }
+//     },
+//     scene: [
+//         // SceneMainMenu,
+//         // playerScript,
+//         // SceneGameOver
+        
+//     ],
+//         preload: preload,
+//         create: create,
+//         update: update
+//     ,
+//     audio:{  
+
+//       }
+// };
+
+
+
+
+
+// var game = new Phaser.Game(config);
